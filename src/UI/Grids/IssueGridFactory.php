00578a72ed41aca2bfeb6b80581296cc279c0ad0
Jakub Janata <jakubjanata@gmail.com>
2017-09-01 07:34:34 +0200

    added grid filters

--- src/UI/Grids/IssueGridFactory.php
+++ src/UI/Grids/IssueGridFactory.php
@@ -37,19 +37,65 @@ class IssueGridFactory
         $grid = new DataGrid;
         $grid->setDataSource($issueService->getAllIssues());
 
-        $grid->addColumnText('id', 'ID');
+        $grid->addColumnText('id', 'ID')
+            ->setFilterText()
+            ->setCondition(function (array $issues, $id) {
+                return array_filter($issues, function (Issue $issue) use ($id) {
+                    return strpos((string) $issue->getId(), $id) !== false;
+                });
+            });
 
-        $grid->addColumnText('tracker', 'Typ', 'tracker.name');
+        $grid->addColumnText('tracker', 'Typ', 'tracker.name')
+            ->setFilterSelect([null => 'Vše'] + $issueService->getTrackersList())
+            ->setCondition(function (array $issues, $id) {
+                return array_filter($issues, function (Issue $issue) use ($id) {
+                    return $issue->getTrackerId() === (int) $id;
+                });
+            });
 
-        $grid->addColumnText('subject', 'Název');
+        $grid->addColumnText('subject', 'Název')
+            ->setFilterText()
+            ->setCondition(function (array $issues, $text) {
+                return array_filter($issues, function (Issue $issue) use ($text) {
+                    return stripos($issue->getSubject(), $text) !== false;
+                });
+            });
 
-        $grid->addColumnText('status', 'Stav', 'status.name');
+        $grid->addColumnText('status', 'Stav', 'status.name')
+            ->setFilterSelect([null => 'Vše'] + $issueService->getStatusesList())
+            ->setCondition(function (array $issues, $id) {
+                return array_filter($issues, function (Issue $issue) use ($id) {
+                    return $issue->getStatusId() === (int) $id;
+                });
+            });
 
-        $grid->addColumnText('priority', 'Priorita', 'priority.name');
+        $grid->addColumnText('priority', 'Priorita', 'priority.name')
+            ->setFilterSelect([null => 'Vše'] + $issueService->getPrioritiesList())
+            ->setCondition(function (array $issues, $id) {
+                return array_filter($issues, function (Issue $issue) use ($id) {
+                    return $issue->getPriorityId() === (int) $id;
+                });
+            });
 
-        $grid->addColumnText('author', 'Autor', 'author.name');
+        $grid->addColumnText('author', 'Autor', 'author.name')
+            ->setFilterText()
+            ->setCondition(function (array $issues, $text) {
+                return array_filter($issues, function (Issue $issue) use ($text) {
+                    return stripos($issue->getAuthor()->getName(), $text) !== false;
+                });
+            });
 
-        $grid->addColumnText('assignedTo', 'Řeší', 'assignedTo.name');
+        $grid->addColumnText('assignedTo', 'Řeší', 'assignedTo.name')
+            ->setFilterText()
+            ->setCondition(function (array $issues, $text) {
+                return array_filter($issues, function (Issue $issue) use ($text) {
+                    if ($issue->getAssignedTo() === null) {
+                        return false;
+                    }
+
+                    return stripos($issue->getAssignedTo()->getName(), $text) !== false;
+                });
+            });
 
         $grid->addColumnText('doneRatio', 'Hotovo', 'doneRatio')
             ->setRenderer(function (Issue $issue) {
